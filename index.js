67d9061c5e34d16bf5b0a39d3ae83714777f4aa8
johnnybluhm <54964159+johnnybluhm@users.noreply.github.com>
2022-01-05 22:17:03 -0700

    Add final work for night, rough

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
-const puppeteer = require('puppeteer-extra');
-const StealthPlugin = require('puppeteer-extra-plugin-stealth');
-puppeteer.use(StealthPlugin())
+const puppeteer = require('puppeteer');
+const fs = require("fs");
+/*const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+puppeteer.use(StealthPlugin())*/
 
 const start = async () => {
-  /*const args = [
-    '--no-sandbox',
-    '--disable-setuid-sandbox',
-    '--disable-infobars',
-    '--window-position=0,0',
-    '--ignore-certifcate-errors',
-    '--ignore-certifcate-errors-spki-list',
-    '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"'
-  ];
-  const options = {
-    args,
-    headless: false
-  };
-
-  const browser = await puppeteer.launch(options);*/
+  deleteOldScreenshots();
   const browser = await puppeteer.launch({headless: false});
   const page = (await browser.pages())[0];
 
-  /*page.on('request', async () => {
-    await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36')
-  })*/
-
-  //await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36')
   await page.goto('https://www.kingsoopers.com/signin/');
 
-  await page.screenshot({ path: './images/initialLogin.png' })
+  //await page.screenshot({ path: './screenshots/initialLogin.png' })
 
   await page.type("#SignIn-emailInput", "jon.the.bon.bon@gmail.com")
   await page.type("#SignIn-passwordInput", "hFvGf6uV_P#FwyF")
-  await page.screenshot({ path: './images/loginafterTyping.png' })
+  //await page.screenshot({ path: './screenshots/loginafterTyping.png' })
 
-  await Promise.all([page.click("#SignIn-submitButton"), page.waitForNavigation()])
+  const res = await Promise.all([page.click("#SignIn-submitButton"), page.waitForNavigation()])
 
-  await page.screenshot({ path: './images/afterNav.png' })
+  
+  //await page.screenshot({ path: './screenshots/afterNav.png' })
+
+  /*while(page.$("#SignIn-submitButton")!=null){
+    await Promise.all([page.click("#SignIn-submitButton"), page.waitForNavigation()])
+  }*/
+
+  await page.goto('https://www.kingsoopers.com/mypurchases');
+
+  const links = await page.$$eval('a[data-testid="order-details-link"]', (anchors) =>{
+    return anchors.map(anchor => anchor.href);
+  });
+
+  links.forEach((a)=>{
+    console.log(a);
+  })
+
+  console.log(links);
+
   await browser.close();
 }
 
@@ -52,6 +52,17 @@ const google = async () => {
   await Promise.all([page.click("div.FPdoLc.lJ9FBc input.gNO89b"), page.waitForNavigation({waitUntil: 'networkidle2'})])
   await page.screenshot({ path: 'afterLogin.png' })
   //await browser.close();
+}
+const deleteOldScreenshots = () => {
+  const dir = "screenshots"
+  try {
+    fs.rmdirSync(dir, { recursive: true });
+    console.log(`${dir} dir deleted!`);
+    fs.mkdirSync("screenshots")
+    console.log(`fresh ${dir} dir created!`);
+} catch (err) {
+    console.error(`Error while deleting ${dir}.`);
+}
 }
 
 //google();
