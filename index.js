d3ea84a12161902d6960540c2963a3370edb3538
johnnybluhm <54964159+johnnybluhm@users.noreply.github.com>
2022-01-08 13:39:46 -0700

    Add almost working version

--- index.js
+++ index.js
@@ -5,92 +5,131 @@ const { executablePath } = require('puppeteer');
 puppeteer.use(StealthPlugin())
 puppeteer.use(require('puppeteer-extra-plugin-anonymize-ua')())
 
-const path = require('path');
-
 const start = async (delete_) => {
-  if(delete_){
-    deleteOldOrders();
-  }  
-  const browser = await getBrowser();
-  const initialPage = await getInitialPage();
-  await navigateToMyPurchases();  
+    if (delete_) {
+        deleteOldOrders();
+    }
+    var context = {
+        browser : null,
+        intialPage : null,
+    }
+    const browser = await getBrowser();
+    const initialPage = await getInitialPage();
+    const purchasesPage = await navigateToMyPurchases();
 
-  const orderDetailsLinks = await initialPage.$$eval('a[data-testid="order-details-link"]', async (anchors) =>{
-    return anchors.map(anchor => anchor.href);
-  });
-
-  var newPagePromises = new Array();
-  orderDetailsLinks.forEach(()=>{
-    newPagePromises.push(browser.newPage())
-  })
-  const orderPages = await Promise.all(newPagePromises);  
-
-  var pageGotoPromises = [];
-  orderPages.forEach((orderPage, index) => {
-    pageGotoPromises.push(orderPage.goto(orderDetailsLinks[index],{waitUntil: 'domcontentloaded'} ))
-  })
-
-  await Promise.all(pageGotoPromises)
-
-  var orderSelectPromises = new Array()
-  orderPages.forEach((orderPage) => {
-    orderSelectPromises.push(orderPage.$$('div.PH-ProductCard-container.w-full.p-16'))
-    console.log(orderSelectPromises)
-  })  
-  const orders = await Promise.all(orderSelectPromises)
-  orders.forEach((order, num)=>{
-    var itemString = "";
-    order.forEach((item)=> {
-      itemString +=`${item}+\n`
-    })
-    fs.write(`./orders/order-${num}`, itemString)
-  })
-  await browser.close();
-
-  //HELPER CLOSURES:
-
-  const getBrowser = async () => {
-    const browser_path = 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe';
-    const options = {executablePath: browser_path, headless: false}
-    const browser = await puppeteer.launch(options);
-    return browser;
-  }
-  
-  const getInitialPage = async () => {
+    const orderDetailsLinks = await getOrderPageLinks()
     
-    const page = (await browser.pages())[0];
-    /*page.on('dialog', async dialog => {
-      console.log(dialog.message());
-      await dialog.dismiss();
-    });*/
-  
-    await page.goto('https://www.kingsoopers.com/signin/');
-  }
+    const orderPages = await createOrderPages();
 
-  const navigateToMyPurchases = async () => {
+    await navigateOrderPagesToOrderDetails();
+
+    const orders = await getOrders();
+
+    writeOrdersToDisk();
+
+    await browser.close();
+
+    //HELPER CLOSURES:
+    async function getBrowser() {
+        const browser_path = 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe';
+        //const options = { executablePath: browser_path, headless: false }
+        const browser = await puppeteer.launch({headless:false});
+        return browser;
+    }
+
+    async function getInitialPage() {
+
+        const page = (await browser.pages())[0];
+        /*page.on('dialog', async dialog => {
+          console.log(dialog.message());
+          await dialog.dismiss();
+        });*/
     
-  await initialPage.type("#SignIn-emailInput", "jon.the.bon.bon@gmail.com")
-  await initialPage.type("#SignIn-passwordInput", "hFvGf6uV_P#FwyF")
+        await page.goto('https://www.kingsoopers.com/signin/');
+    
+        return page;
+    }
 
-  await Promise.all([initialPage.click("#SignIn-submitButton"), initialPage.waitForNavigation()])
+    async function navigateToMyPurchases(){
 
-  await initialPage.goto('https://www.kingsoopers.com/mypurchases');
-  }
-}
+        await initialPage.type("#SignIn-emailInput", "jon.the.bon.bon@gmail.com")
+        await initialPage.type("#SignIn-passwordInput", "hFvGf6uV_P#FwyF")
+    
+        await Promise.all([initialPage.click("#SignIn-submitButton"), initialPage.waitForNavigation()])
+    
+        await initialPage.goto('https://www.kingsoopers.com/mypurchases');
+    
+        return initialPage;
+    }
 
+    async function getOrderPageLinks() {
+        return await purchasesPage.$$eval('a[data-testid="order-details-link"]', async (anchors) => {
+            return await anchors.map(anchor => anchor.href);
+        });
+    }
+    
+    async function createOrderPages(){
+    
+        var newPagePromises = new Array();
+        orderDetailsLinks.forEach(() => {
+            newPagePromises.push(browser.newPage())
+        })
+    
+        return await Promise.all(newPagePromises);
+    }
+    
+    async function navigateOrderPagesToOrderDetails(){
+    
+        var pageGotoPromises = [];
+        orderPages.forEach((orderPage, index) => {
+            pageGotoPromises.push(orderPage.goto(orderDetailsLinks[index], { waitUntil: 'load' }))
+        })
+    
+        return await Promise.all(pageGotoPromises)
+    }
+    
+    async function getOrders(){
+    
+        var orderSelectPromises = new Array()
+        orderPages.forEach((orderPage) => {
+            orderSelectPromises.push(orderPage.$$('div.PH-ProductCard-container.w-full.p-16'))
+        })
+    
+        return await Promise.all(orderSelectPromises)
+    }
+    
+    async function writeOrdersToDisk(){
+    
+        orders.forEach(async (order, num) => {
+            var itemString = "";
 
+            order.forEach(async (item) => {
+                var elementJson = await item.jsonValue();
+                var elementString = elementJson.toString();
+
+                itemString += `${elementString}+\n`
+            })
+
+            await fs.writeFile(`./orders/order-${num}`, itemString, (err)=> {
+                if(err){
+                    console.log(itemString)
+                    console.log(err)
+                }
+            })
+        })
+    }
+}//start
 
 const deleteOldOrders = () => {
-  const dir = "orders"
-  try {
-    fs.rmdirSync(dir, { recursive: true });
-    console.log(`${dir} dir deleted!`);
-    fs.mkdirSync("orders")
-    console.log(`fresh ${dir} dir created!`);
-} catch (err) {
-    console.error(`Error while deleting ${dir}.`);
+    const dir = "orders"
+    try {
+        fs.rmdirSync(dir, { recursive: true });
+        console.log(`${dir} dir deleted!`);
+        fs.mkdirSync("orders")
+        console.log(`fresh ${dir} dir created!`);
+    } catch (err) {
+        console.error(`Error while deleting ${dir}.`);
+    }
 }
-}
-
 
 start(true)
