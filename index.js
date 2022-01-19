eae88ee2c5e79dd596c5dcafd80eec2dca258c4d
johnnybluhm <54964159+johnnybluhm@users.noreply.github.com>
2022-01-18 20:44:56 -0700

    Clean up code

--- index.js
+++ index.js
@@ -1,135 +1,18 @@
-const puppeteer = require('puppeteer-extra');
+const puppeteer = require('puppeteer');
 const fs = require("fs");
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 const { executablePath } = require('puppeteer');
-puppeteer.use(StealthPlugin())
+//puppeteer.use(StealthPlugin())
 //puppeteer.use(require('puppeteer-extra-plugin-anonymize-ua')())
 
-const start = async (delete_) => {
-    if (delete_) {
-        deleteOldOrders();
-    }
+async function getOrders(){
+
     const browser = await getBrowser();
     const initialPage = await getInitialPage();
-    const purchasesPage = await navigateToMyPurchases();
-
-    const orderDetailsLinks = await getOrderPageLinks()
-    
-    const orderPages = await createOrderPages();
-
-    await navigateOrderPagesToOrderDetails();
-
-    const orders = await getOrders();
-
-    writeOrdersToDisk();
-
+    await navigateToMyPurchases();
     await browser.close();
 
-    //HELPER CLOSURES:
-    async function getBrowser() {
-        const browser_path = 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe';
-        const firefox_path = "C:\\Program Files\\Mozilla Firefox\\firefox.exe";
-        const options = { executablePath: browser_path, headless: false, product: 'chrome' }
-        const browser = await puppeteer.launch(options);
-        return browser;
-    }
-
-    async function getInitialPage() {
-
-        const page = (await browser.pages())[0];
-        await page.goto('https://www.kingsoopers.com/signin/');
-    
-        return page;
-    }
-
-    async function navigateToMyPurchases(){
-
-        await initialPage.type("#SignIn-emailInput", "jon.the.bon.bon@gmail.com")
-        await initialPage.type("#SignIn-passwordInput", "hFvGf6uV_P#FwyF")
-    
-        await Promise.all([initialPage.click("#SignIn-submitButton"), initialPage.waitForNavigation()])
-    
-        await initialPage.goto('https://www.kingsoopers.com/mypurchases');
-
-        let purchasesPage = initialPage;
-        return purchasesPage;
-    }
-
-    async function getOrderPageLinks() {
-        return await purchasesPage.$$eval('a[data-testid="order-details-link"]', anchors => anchors.map(anchor => anchor.href));
-    }
-    
-    async function createOrderPages(){
-    
-        var newPagePromises = new Array();
-        orderDetailsLinks.forEach(() => {
-            newPagePromises.push(browser.newPage())
-        })
-    
-        return await Promise.all(newPagePromises);
-    }
-    
-    async function navigateOrderPagesToOrderDetails(){
-    
-        var pageGotoPromises = [];
-        orderPages.forEach((orderPage, index) => {
-            pageGotoPromises.push(orderPage.goto(orderDetailsLinks[index], { waitUntil: 'load' }))
-        })
-    
-        return await Promise.all(pageGotoPromises)
-    }
-    
-    async function getOrders(){
-    
-        var orderSelectPromises = new Array()
-        orderPages.forEach((orderPage) => {
-            orderSelectPromises.push(orderPage.$$('div.PH-ProductCard-container.w-full.p-16'))
-        })
-    
-        return await Promise.all(orderSelectPromises)
-    }
-    
-    async function writeOrdersToDisk(){
-    
-        orders.forEach(async (order, num) => {
-            var itemString = "";
-
-            order.forEach(async (item) => {
-                var elementJson = await item.jsonValue();
-                var elementString = elementJson.toString();
-
-                itemString += `${elementString}+\n`
-            })
-
-            await fs.writeFile(`./orders/order-${num}`, itemString, (err)=> {
-                if(err){
-                    console.log(itemString)
-                    console.log(err)
-                }
-            })
-        })
-    }
-}//start
-
-const deleteOldOrders = () => {
-    const dir = "orders"
-    try {
-        fs.rmdirSync(dir, { recursive: true });
-        console.log(`${dir} dir deleted!`);
-        fs.mkdirSync("orders")
-        console.log(`fresh ${dir} dir created!`);
-    } catch (err) {
-        console.error(`Error while deleting ${dir}.`);
-    }
-}
-
-
-async function getRequests(){
-
-    const browser = await getBrowser();
-    const initialPage = await getInitialPage();
-    await signIn();
-
+    console.log("We got the data")
 
     async function getBrowser() {
         //const browser_path = 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe';
@@ -142,31 +25,41 @@ async function getRequests(){
 
         const page = (await browser.pages())[0];
         await page.goto('https://www.kingsoopers.com/signin/');
-        page.on('response', logRequest)
+        page.on('response', checkRequest)
         return page;
     }
 
-    async function signIn(){
+    async function navigateToMyPurchases(){
         await initialPage.type("#SignIn-emailInput", "jon.the.bon.bon@gmail.com")
-        await initialPage.type("#SignIn-passwordInput", "hFvGf6uV_P#FwyF")    
-        await Promise.all([initialPage.click("#SignIn-submitButton"), initialPage.waitForNavigation()])
-    }
-}
-
-async function logRequest(interceptedRequest) {
-    if(interceptedRequest.url() == "https://www.kingsoopers.com/products/api/products/details-basic"){
-        console.log('A request was made:', interceptedRequest.url());
-        const data = await interceptedRequest.json();
-        console.log(data);
-        writeJsonToDisk(data);
-    }
-}
-
-async function writeJsonToDisk(data){
-    const str = JSON.stringify(data)
-    const time = new Date();
+        await initialPage.type("#SignIn-passwordInput", "hFvGf6uV_P#FwyF")
     
-    await fs.writeFile(`./orders/order-${time.toDateString()}`, str)
+        await Promise.all([initialPage.click("#SignIn-submitButton"), initialPage.waitForNavigation()])
+    
+        await initialPage.goto('https://www.kingsoopers.com/mypurchases');
+    }
 }
 
-getRequests();
+async function checkRequest(interceptedRequest) {
+    const desired_api_request = "https://www.kingsoopers.com/mypurchases/api/v1/receipt/details";
+    if(interceptedRequest.url() == desired_api_request){
+        console.log('Ajax request made to desired api:', interceptedRequest.url());
+        const purchaseHistory = await interceptedRequest.json();
+        console.log(purchaseHistory);
+        writeJsonToDisk(purchaseHistory);
+    }
+}
+
+async function writeJsonToDisk(purchaseHistory){
+    const history = JSON.stringify(purchaseHistory)
+    const time = new Date().toDateString();
+    
+    await fs.writeFile(`./orders/order-${time}`, history, (err) => {
+        if (err)
+          console.log(err);
+        else {
+          console.log("File written successfully\n");
+        }
+      });
+}
+
+getOrders();
